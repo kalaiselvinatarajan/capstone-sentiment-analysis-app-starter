68b9db077d2441969bf196e2d9da0a0432c4bd51
kalaiselvinatarajan <kalaiselvi.natarajan@gmail.com>
2025-02-11 04:14:08 +0000

    First Approach1

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
+    print("Hi")
+    sentiment = None
     # TODO: Write the code that calls the sentiment analysis functions here.
-    # hint: use request.method == "POST"
-    return render_template('form.html')
+    if request.method == "POST" :
+        ext = request.form.get("user_text")
+        sid = SentimentIntensityAnalyzer()
+        sentiment = sid.polarity_scores(ext)
+        print(sentiment)
+    return render_template('form.html',sentiment=sentiment)
 if __name__ == "__main__":
     app.run()
